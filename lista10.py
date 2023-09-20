5ac3a12f1848f6421d4f81599c0e2b7d6a364b94
20221tpmi0165 <134156746+20221tpmi0165@users.noreply.github.com>
2023-09-20 16:24:52 -0300

    Add files via upload

---
+++ lista10.py
@@ -0,0 +1,18 @@
+nume_pares = 0
+nume_pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in nume_pares:
+    if nume_pares[i] % 2 == 0:
+        nume_pares[i] = 0
+        print(nume_pares)
+
+
+
+#
+
+
+
+
+  
+    
+        
+        
