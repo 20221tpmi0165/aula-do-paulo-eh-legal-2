9cb75a02d154c3d2e90735e6f91812b544332969
20221tpmi0165 <134156746+20221tpmi0165@users.noreply.github.com>
2023-09-22 13:16:23 -0300

    Add files via upload

---
+++ ExlIsta.py/exlis1.py
@@ -0,0 +1,6 @@
+numeros_pares = []
+for x in range(0,10):
+    if x % 2 == 0:
+        numeros_pares.append(x)
+
+print (numeros_pares)
---
+++ ExlIsta.py/exlis9.py
@@ -0,0 +1,8 @@
+lista_total = []
+lista1 = [1,3,4,6,7,]
+lista2 = [1,2,4,5,8,]
+for i in lista1:
+    for j in lista2:
+        if (i == j):
+            print(i)
+            break
